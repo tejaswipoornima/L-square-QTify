70ebb0e6fb141fdbd854a26781b9920d148f7b24
Tejaswi Poornima <tejaswipoornima@gmail.com>
2023-11-28 18:40:16 +0530

    navbar

---
+++ qtify/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "./Navbar.module.css"
+import Button from "../Button/Button";
+import Logo from "../Logo/Logo";
+import Search from "../Search/Search";
+
+function Navbar(){
+    return (
+        <nav className={styles.navbar}>
+            <a href="/">
+                <Logo/>
+            </a>
+            <Search placeholder="Search a song of your choice"/>
+            <Button>Give Feedback</Button>
+        </nav>
+    )
+}
+
+export default Navbar;
---
+++ qtify/src/components/Search/Search.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import {ReactComponent as SearchIcon} from "../../assets/Search-icon.svg"
+import styles from "./Search.module.css"
+
+function Search({placeholder}){
+    const onSubmit =(e)=>{
+        e.preventDefault()
+        //process form data here
+    }
+    return (
+        <form className={styles.wrapper}onSubmit={onSubmit}>
+            <input className={styles.search} placeholder={placeholder} required/>
+            <button className={styles.searchButton} type="submit">
+                <SearchIcon/>
+            </button>
+        </form>
+    )
+}
+
+export default Search;
