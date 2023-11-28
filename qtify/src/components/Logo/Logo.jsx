ee2949a959b35a67a9119747bc4157b5c24ac034
Tejaswi Poornima <tejaswipoornima@gmail.com>
2023-11-28 20:21:38 +0530

    navbar2

--- qtify/src/components/Logo/Logo.jsx
+++ qtify/src/components/Logo/Logo.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import LogoImage from "../../assets/logo.png"
+import styles from "./Logo.module.css"
 
 export default function Logo(){
-    return <img src={LogoImage} alt="logo" width={67}/>
+    return <img className={styles.logo} src={LogoImage} alt="logo"/>
 }
